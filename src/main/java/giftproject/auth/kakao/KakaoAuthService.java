e81179913007100fee6665c11e39e3c759120280
gosurim <su020900@gmail.com>
2025-07-24 03:14:48 +0900

    feat: 액세스 토큰 획득 AuthService 작성

---
+++ src/main/java/giftproject/auth/kakao/KakaoAuthService.java
@@ -0,0 +1,40 @@
+package giftproject.auth.kakao;
+
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class KakaoAuthService {
+
+    @Value("${kakao.client-id}")
+    private String clientId;
+
+    @Value("${kakao.redirect-uri}")
+    private String redirectUri;
+
+    private final RestClient restClient = RestClient.create();
+
+    public String getKakaoAccessToken(String authorizationCode){
+        String url = "https://kauth.kakao.com/oauth/token";
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "authorization_code");
+        body.add("client_id", clientId);
+        body.add("redirect_uri", redirectUri);
+        body.add("code", authorizationCode);
+
+        Map<String, Object> responseBody = restClient.post()
+                .uri(url)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(body)
+                .retrieve()
+                .body(Map.class);
+
+        return responseBody.get("access_token").toString();
+    }
+}
